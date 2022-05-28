c86e69f5e04c659a6e15d75f591e49254575609f
kenanatasoy <kenanatasoy@outlook.com>
2022-05-28 19:05:44 +0300

    added isBalancedParentheses challenge

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,18 +1,16 @@
 package org.example;
 
-import org.example.practices.RepeatingLettersUsingMarking;
-
 /**
  * Hello world!
  *
  */
 
-public class App 
+public class App
 {
-    public static void main( String[] args ) {
 
-        RepeatingLettersUsingMarking repeatingLettersUsingMarking = new RepeatingLettersUsingMarking();
-        System.out.println(repeatingLettersUsingMarking.findRepeatingLettersUsingMarking("fdgfdgsdbtybsdşskhs"));
+    public static void main( String[] args ) throws Exception {
+
+
 
     }
 }
---
+++ src/main/java/org/example/practices/IsBalancedParentheses.java
@@ -0,0 +1,47 @@
+package org.example.practices;
+
+import java.util.*;
+
+class IsBalancedParentheses{
+
+    private boolean isBalanced(String string){
+
+        if (string.length() % 2 != 0)
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> charMap = new HashMap<>();
+
+        charMap.put('(', ')');
+        charMap.put('{', '}');
+        charMap.put('[', ']');
+        charMap.put(')', '(');
+        charMap.put('}', '{');
+        charMap.put(']', '[');
+
+
+        for (int i = 0; i < string.length(); i++) {
+
+            char reverseChar = charMap.get(string.charAt(i));
+
+            if(isLeftBracket(string.charAt(i)))
+                stack.push(string.charAt(i));
+
+            else if( stack.isEmpty() || stack.pop() != reverseChar )
+                return false;
+
+        }
+
+        return stack.empty();
+
+    }
+
+    private boolean isLeftBracket(char c){
+        return c == '[' || c == '{' || c == '(';
+    }
+
+
+}
+
+
+
